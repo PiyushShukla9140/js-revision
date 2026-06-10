758bfab3d54ad8196e87f91f791aff0bdcb3745b
Piyush Shukla <piyushshukla8980@gmail.com>
2026-06-10 11:21:27 +0530

    completed till loops and areay method

--- 04_ArraysAndObjects/arrays.js
+++ 04_ArraysAndObjects/arrays.js
@@ -1,7 +1,7 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros) iska output thoda ajib aata h
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
@@ -9,17 +9,19 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [...marvel_heros, ...dc_heros]// by using dpread operator ...array_name
+ 
 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
+// this above array function used to convert that given array to single array
 console.log(real_another_array);
 
 
-
+// kai baaar array alag alag formats me bhu aa skta to uke wapas array em alane ke liya
 console.log(Array.isArray("Hitesh"))
 console.log(Array.from("Hitesh"))
 console.log(Array.from({name: "hitesh"})) // interesting
---
+++ 04_ArraysAndObjects/object.js
@@ -0,0 +1,38 @@
+// singleton: jab bhi hum object ko constructor ki trh define krte hao tab voh singleton hota h
+// Object.create yeh method hota h constructor wala
+
+// object literals ki trh define hote htab voh singleton nhi hota
+// yaha literals ke form mme objects ko bnaya gya h
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",// if you dont write in square brackets it wont refer to the varibale defined above
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
